c5ad5a848c995dda3812c6b893c4a71b04ee836b
Karthik Desingu <karthikdesingu2000@gmail.com>
2024-05-12 18:42:45 -0400

    surafec intersection waves

---
+++ project_mitotic-trigger-waves-xenopus/surface_contraction_waves.m
@@ -0,0 +1,10 @@
+function funcs = surface_contraction_waves
+    funcs.surface_intersection_eqns = @surface_intersection_eqns;
+end
+
+
+function [sphere_surface_eqn, wave_eqn] = surface_intersection_eqns
+    syms x y v rc t x0 y0 t0;
+    sphere_surface_eqn = y^2 + x^2 == rc^2;
+    wave_eqn = (y-y0)^2 + (x-x0)^2 == v^2*(t-t0)^2;
+end
