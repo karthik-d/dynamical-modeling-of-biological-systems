c3bf0c1246bb9790729be109513bc4a94ca89df7
Karthik Desingu <karthikdesingu2000@gmail.com>
2024-05-12 22:27:52 -0400

    effect of pos fb on oscillations

--- project_mitotic-trigger-waves-xenopus/cdk1_cyclin_ode.m
+++ project_mitotic-trigger-waves-xenopus/cdk1_cyclin_ode.m
@@ -27,17 +27,17 @@ end
 
 function dydt = cdk1_cyclin_ode_dydt(t, y, params, consts)
 
-    syms ksynth acdc25 bcdc25 ncdc25 awee1 bwee1 nwee1 adeg bdeg ndeg EC50cdc25 EC50wee1 EC50deg Cdk1 Cyc;
+    syms ksynth acdc25 bcdc25 ncdc25 awee1 bwee1 nwee1 adeg bdeg ndeg EC50cdc25 EC50wee1 EC50deg Cdk1 Cyc pos_fb_strength;
 
     % dydt is a vector of rates for [Cdk1; Cyclin].
     [cdk1_expr, cyclin_expr] = cdk1_cyclin_ode_exprs;
-    dydt = [ double(subs(cdk1_expr, {ksynth acdc25 bcdc25 ncdc25 awee1 bwee1 nwee1 adeg bdeg ndeg EC50cdc25 EC50wee1 EC50deg Cdk1 Cyc}, ...
+    dydt = [ double(subs(cdk1_expr, {ksynth acdc25 bcdc25 ncdc25 awee1 bwee1 nwee1 adeg bdeg ndeg EC50cdc25 EC50wee1 EC50deg Cdk1 Cyc pos_fb_strength}, ...
                 {params.ksynth params.acdc25 params.bcdc25 params.ncdc25 params.awee1 params.bwee1 params.nwee1 params.adeg params.bdeg params.ndeg ...
-                consts.EC50cdc25 consts.EC50wee1 consts.EC50deg y(1) y(2)})) ...
+                consts.EC50cdc25 consts.EC50wee1 consts.EC50deg y(1) y(2) params.pos_fb_strength})) ...
 		; ...
-            double(subs(cyclin_expr, {ksynth acdc25 bcdc25 ncdc25 awee1 bwee1 nwee1 adeg bdeg ndeg EC50cdc25 EC50wee1 EC50deg Cdk1 Cyc}, ...
+            double(subs(cyclin_expr, {ksynth acdc25 bcdc25 ncdc25 awee1 bwee1 nwee1 adeg bdeg ndeg EC50cdc25 EC50wee1 EC50deg Cdk1 Cyc pos_fb_strength}, ...
                 {params.ksynth params.acdc25 params.bcdc25 params.ncdc25 params.awee1 params.bwee1 params.nwee1 params.adeg params.bdeg params.ndeg ...
-                consts.EC50cdc25 consts.EC50wee1 consts.EC50deg y(1) y(2)}))
+                consts.EC50cdc25 consts.EC50wee1 consts.EC50deg y(1) y(2) params.pos_fb_strength}))
         ];
 end
 
@@ -45,23 +45,23 @@ function dydt = cdk1_cyclin_ode_dydt_array(t, y, params, consts)
 
     % serves the same purpose as `cdk1_cyclin_ode_dydt` but takes y as a 3D
     % array and returns dydt as a 3D array --> helper for generating vector field.
-    syms ksynth acdc25 bcdc25 ncdc25 awee1 bwee1 nwee1 adeg bdeg ndeg EC50cdc25 EC50wee1 EC50deg Cdk1 Cyc;
+    syms ksynth acdc25 bcdc25 ncdc25 awee1 bwee1 nwee1 adeg bdeg ndeg EC50cdc25 EC50wee1 EC50deg Cdk1 Cyc pos_fb_strength;
 
     % dydt is a vector of rates for [Cdk1; Cyclin].
     [cdk1_expr, cyclin_expr] = cdk1_cyclin_ode_exprs;
-    dydt = double(subs(cdk1_expr, {ksynth acdc25 bcdc25 ncdc25 awee1 bwee1 nwee1 adeg bdeg ndeg EC50cdc25 EC50wee1 EC50deg Cdk1 Cyc}, ...
+    dydt = double(subs(cdk1_expr, {ksynth acdc25 bcdc25 ncdc25 awee1 bwee1 nwee1 adeg bdeg ndeg EC50cdc25 EC50wee1 EC50deg Cdk1 Cyc pos_fb_strength}, ...
                 {params.ksynth params.acdc25 params.bcdc25 params.ncdc25 params.awee1 params.bwee1 params.nwee1 params.adeg params.bdeg params.ndeg ...
-                consts.EC50cdc25 consts.EC50wee1 consts.EC50deg y(:, :, 1) y(:, :, 2)}));
+                consts.EC50cdc25 consts.EC50wee1 consts.EC50deg y(:, :, 1) y(:, :, 2) params.pos_fb_strength}));
     % append to third axis.
-	dydt = cat(3, dydt, double(subs(cyclin_expr, {ksynth acdc25 bcdc25 ncdc25 awee1 bwee1 nwee1 adeg bdeg ndeg EC50cdc25 EC50wee1 EC50deg Cdk1 Cyc}, ...
+	dydt = cat(3, dydt, double(subs(cyclin_expr, {ksynth acdc25 bcdc25 ncdc25 awee1 bwee1 nwee1 adeg bdeg ndeg EC50cdc25 EC50wee1 EC50deg Cdk1 Cyc pos_fb_strength}, ...
                 {params.ksynth params.acdc25 params.bcdc25 params.ncdc25 params.awee1 params.bwee1 params.nwee1 params.adeg params.bdeg params.ndeg ...
-                consts.EC50cdc25 consts.EC50wee1 consts.EC50deg y(:, :, 1) y(:, :, 2)})));
+                consts.EC50cdc25 consts.EC50wee1 consts.EC50deg y(:, :, 1) y(:, :, 2) params.pos_fb_strength})));
 end
 
 
 function [cdk1_val_pts, cyclin_val] = cdk1_cyclin_ode_nullclines(params, consts, cyc_vals, hyst_range)
 
-	syms ksynth acdc25 bcdc25 ncdc25 awee1 bwee1 nwee1 adeg bdeg ndeg EC50cdc25 EC50wee1 EC50deg Cdk1 Cyc;
+	syms ksynth acdc25 bcdc25 ncdc25 awee1 bwee1 nwee1 adeg bdeg ndeg EC50cdc25 EC50wee1 EC50deg Cdk1 Cyc pos_fb_strength;
 	syms rate;
 
 	% set up helpers.
@@ -74,9 +74,9 @@ function [cdk1_val_pts, cyclin_val] = cdk1_cyclin_ode_nullclines(params, consts,
     cdk1_val_pts = zeros([length(cyc_vals) 2]);
     for k=1:length(cyc_vals)
         cdk1_eqn = compose(null_eqn, cdk1_expr);
-        cdk1_eqn = subs(cdk1_eqn, {ksynth acdc25 bcdc25 ncdc25 awee1 bwee1 nwee1 adeg bdeg ndeg EC50cdc25 EC50wee1 EC50deg Cyc}, ...
+        cdk1_eqn = subs(cdk1_eqn, {ksynth acdc25 bcdc25 ncdc25 awee1 bwee1 nwee1 adeg bdeg ndeg EC50cdc25 EC50wee1 EC50deg Cyc pos_fb_strength}, ...
                     {params.ksynth params.acdc25 params.bcdc25 params.ncdc25 params.awee1 params.bwee1 params.nwee1 params.adeg params.bdeg params.ndeg ...
-                    consts.EC50cdc25 consts.EC50wee1 consts.EC50deg cyc_vals(k)});
+                    consts.EC50cdc25 consts.EC50wee1 consts.EC50deg cyc_vals(k) params.pos_fb_strength});
         cdk1_val_pts(k, 2) = vpasolve(cdk1_eqn, Cdk1);
         cdk1_val_pts(k, 1) = cyc_vals(k);
     end
@@ -91,9 +91,9 @@ function [cdk1_val_pts, cyclin_val] = cdk1_cyclin_ode_nullclines(params, consts,
 
         % set up the equation.
         cdk1_eqn = compose(null_eqn, cdk1_expr);
-        cdk1_eqn = subs(cdk1_eqn, {ksynth acdc25 bcdc25 ncdc25 awee1 bwee1 nwee1 adeg bdeg ndeg EC50cdc25 EC50wee1 EC50deg Cyc}, ...
+        cdk1_eqn = subs(cdk1_eqn, {ksynth acdc25 bcdc25 ncdc25 awee1 bwee1 nwee1 adeg bdeg ndeg EC50cdc25 EC50wee1 EC50deg Cyc pos_fb_strength}, ...
                     {params.ksynth params.acdc25 params.bcdc25 params.ncdc25 params.awee1 params.bwee1 params.nwee1 params.adeg params.bdeg params.ndeg ...
-                    consts.EC50cdc25 consts.EC50wee1 consts.EC50deg addnl_val});
+                    consts.EC50cdc25 consts.EC50wee1 consts.EC50deg addnl_val params.pos_fb_strength});
         
         % get first soln; closest to zero.
         first_soln = vpasolve(cdk1_eqn, Cdk1, 0);
@@ -126,12 +126,12 @@ end
 
 % Helper function that renders the ODEs as symbolic equations.
 function [cdk1_expr, cyclin_expr] = cdk1_cyclin_ode_exprs
-    syms ksynth acdc25 bcdc25 ncdc25 awee1 bwee1 nwee1 adeg bdeg ndeg EC50cdc25 EC50wee1 EC50deg Cdk1 Cyc;
+    syms ksynth acdc25 bcdc25 ncdc25 awee1 bwee1 nwee1 adeg bdeg ndeg EC50cdc25 EC50wee1 EC50deg Cdk1 Cyc pos_fb_strength;
    
     % sym_exprs is a vector of rate equations for [Cdk1; Cyclin].
     cdk1_expr = ksynth ...
-		+ (acdc25 + bcdc25*(Cdk1^ncdc25)/(EC50cdc25^ncdc25 + Cdk1^ncdc25))*(Cyc-Cdk1) ...
-		- (awee1 + bwee1*(EC50wee1^nwee1)/(EC50wee1^nwee1 + Cdk1^nwee1))*Cdk1 ...
+		+ pos_fb_strength*(acdc25 + bcdc25*(Cdk1^ncdc25)/(EC50cdc25^ncdc25 + Cdk1^ncdc25))*(Cyc-Cdk1) ...
+		- pos_fb_strength*(awee1 + bwee1*(EC50wee1^nwee1)/(EC50wee1^nwee1 + Cdk1^nwee1))*Cdk1 ...
 		- (adeg + bdeg*(Cdk1^ndeg)/(EC50deg^ndeg + Cdk1^ndeg))*Cdk1;
 
     cyclin_expr = ksynth ...
